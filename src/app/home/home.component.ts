3b4a4226305f0c3f7aafdc3c259250be545a8c9e
AryanS01 <aryanbest01@gmail.com>
2025-03-01 12:18:31 +0530

    home page redesign

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  slides: any[] = [
+    { image: 'assets/images/slide1.jpg', caption: 'Slide 1' },
+    { image: 'assets/images/slide2.jpg', caption: 'Slide 2' },
+    { image: 'assets/images/slide3.jpg', caption: 'Slide 3' }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
